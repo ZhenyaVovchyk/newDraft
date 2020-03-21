2855b50abf5bbc851f2ddd4889aba93b05dfd436
Zhenya <jen9.vovchyk@outlook.com>
2020-03-21 13:13:52 +0300

    add first files

---
+++ js/ace.js
@@ -0,0 +1,37 @@
+
+let htmlEditor = ace.edit('html-editor');
+htmlEditor.setTheme('ace/theme/monokai');
+htmlEditor.session.setMode('ace/mode/html');
+
+let cssEditor = ace.edit('css-editor');
+cssEditor.setTheme('ace/theme/monokai');
+cssEditor.session.setMode('ace/mode/css');
+
+let jsEditor = ace.edit('js-editor');
+jsEditor.setTheme('ace/theme/monokai');
+jsEditor.session.setMode('ace/mode/javascript')
+
+
+htmlEditor.getSession().on('change', () => {
+    update();
+});
+
+cssEditor.getSession().on('change', () => {
+    update();
+});
+
+jsEditor.getSession().on('change', () => {
+    update();
+});
+
+
+function update() {
+    let res = document.querySelector('#result').contentWindow.document;
+    res.open();
+    res.write(htmlEditor.getValue());
+    res.write('<style>' + cssEditor.getValue() + '</style>');
+    res.write('<script>' + jsEditor.getValue() + '</script>');
+    res.close();
+}
+
+update()
